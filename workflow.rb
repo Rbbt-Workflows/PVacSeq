1cb511ccdb8f87fc8136740348fbeb406f82a9d3
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2019-06-12 15:41:43 +0200

    nil

--- workflow.rb
+++ workflow.rb
@@ -3,8 +3,6 @@ require 'rbbt/workflow'
 
 Misc.add_libdir if __FILE__ == $0
 
-#require 'rbbt/sources/MODULE'
-
 Workflow.require_workflow "VEP"
 module PVacSeq
   extend Workflow
@@ -23,21 +21,17 @@ module PVacSeq
   MHCFLURRY_DOWNLOADS_DIR=Rbbt.share.databases["mhcflurry"].find
   IEDB_INSTALL_DIR=Rbbt.software.opt.IEDB.mhc_i.produce.find
 
-  dep VEP, :analysis, :args_VEP => "--format vcf --pick --symbol --terms SO --plugin Downstream --plugin Wildtype --tsl"
   extension :vcf
-  task :prepare => :text do
+  task :add_sample_info => :text do
     TSV.traverse step(:analysis), :into => :stream, :type => :array, :bar => "Preparing VEP VCF" do |line|
       if line =~ /^#/
         if line =~ /^#CHR/
           line + "\tFORMAT\tSAMPLE"
-        #elsif line =~ /WildtypeProtein/
-        #  line.sub!('WildtypeProtein','WildtypeProtein|DownstreamProtein|ProteinLengthChange')
         else
           line
         end
       else
         next unless line =~ /CSQ=/
-        #next unless %w(A C T G).include? line.split("\t")[4]
         format = "GT"
         sample = "1/1"
         line = line
@@ -46,10 +40,15 @@ module PVacSeq
     end
   end
 
-  extension :vcf
-  dep_task :_prepare, VEP, :analysis, :args_VEP => "--pick --symbol --terms SO --plugin Downstream --plugin Wildtype"
-
-  dep :prepare
+  dep VEP, :analysis, :args_VEP => "--format vcf --pick --symbol --terms SO --plugin Downstream --plugin Wildtype --tsl", :compute => :produce
+  dep :add_sample_info do |jobname,options,dependencies|
+    vep = dependencies.flatten.select{|dep| dep.task_name == :analysis && dep.workflow.to_s == "VEP"}.first
+    if options[:vcf_file]
+      nil
+    else
+      {:input => options, :jobname => jobname}
+    end
+  end
   input :alleles, :array, "Alleles to query"
   task :analysis => :tsv do |alleles|
     target = file('output')
@@ -57,15 +56,38 @@ module PVacSeq
 
     cpus = config(:cpus, :PVacSeq, :pvacseq, :neo_epitopes, :default => 3)
 
+    if dependencies.select{|dep| dep.task_name == :add_sample_info}.any?
+      file = step(:add_sample_info).path
+    else
+      file = step(:analysis).path
+    end
+
+    samples = TSV.open(file).fields[8..-1]
+
+    if samples.length == 1
+      normal_sample = nil
+      tumor_sample = samples.first
+    else
+      normal_sample = samples.select{|s| s =~ /(normal|control|blood|gDNA)/i}.first
+      if normal_sample
+        tumor_sample = (samples - [normal_sample]).last
+      else
+        tumor_sample = samples.last
+      end
+    end
+
     CMD.cmd_log("env MHCFLURRY_DOWNLOADS_DIR=#{MHCFLURRY_DOWNLOADS_DIR} \
-pvacseq run '#{step(:prepare).produce.path}' Test #{alleles * ","} \
-MHCflurry MHCnuggetsI MHCnuggetsII NNalign NetMHC PickPocket SMM SMMPMBEC SMMalign \
+pvacseq run '#{file}' '#{tumor_sample}' #{alleles * ","} \
+MHCflurry MHCnuggetsI MHCnuggetsII NNalign NetMHC NetMHCIIpan NetMHCcons NetMHCpan PickPocket SMM SMMPMBEC SMMalign \
 '#{target}' \
 --iedb-install-directory #{IEDB_INSTALL_DIR}/.. \
+#{normal_sample ? "--normal-sample-name #{normal_sample}" : ""} \
 -t #{cpus} \
+--netmhc-stab \
+--pass-only \
 -e 8,9,10 --binding-threshold 1000000")
 
-    files = file('output').glob('MHC_Class_*/Test.filtered.tsv')
+    files = file('output').glob("MHC_Class_*/#{tumor_sample}.filtered.tsv")
     tsv = files.shift.tsv :header_hash => "", :merge => true
     if files.any?
       tsv = tsv.attach files.first.tsv(:header_hash => "", :merge => true)
@@ -75,9 +97,3 @@ MHCflurry MHCnuggetsI MHCnuggetsII NNalign NetMHC PickPocket SMM SMMPMBEC SMMali
   end
 
 end
-
-#require 'MODULE/tasks/basic.rb'
-
-#require 'rbbt/knowledge_base/MODULE'
-#require 'rbbt/entity/MODULE'
-
